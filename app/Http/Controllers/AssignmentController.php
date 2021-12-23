49582de155b71f85bd0ecb16b647b894e989497a
tomlowen <tom.r.lowen@googlemail.com>
2021-12-23 13:04:13 +0000

    lesson 13

---
+++ app/Http/Controllers/AssignmentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Assignment;
+use App\Http\Requests\StoreAssignmentRequest;
+use App\Http\Requests\UpdateAssignmentRequest;
+
+class AssignmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAssignmentRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAssignmentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Assignment  $assignment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Assignment $assignment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Assignment  $assignment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Assignment $assignment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAssignmentRequest  $request
+     * @param  \App\Models\Assignment  $assignment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAssignmentRequest $request, Assignment $assignment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Assignment  $assignment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Assignment $assignment)
+    {
+        //
+    }
+}

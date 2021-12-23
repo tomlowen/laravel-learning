a02aef569fb60356d97733afe7a500408dcce6d4
tomlowen <tom.r.lowen@googlemail.com>
2021-12-23 12:34:24 +0000

    lesson 10

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use \App\Models\Post;
+
 class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = DB::table('posts')->where('slug', $slug)->first();
+        // $post = Post::where('slug', $slug)->firstOrFail();
+        // $post = DB::table('posts')->where('slug', $slug)->first();
 
-        return view('test', ["name" => $post]);
+        return view('test', ["name" => Post::where('slug', $slug)->firstOrFail()]);
     }
 }

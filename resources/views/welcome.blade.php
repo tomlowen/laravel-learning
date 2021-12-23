4e29cb7d999b79c137bdb3431ac700a79dc3d04b
tomlowen <tom.r.lowen@googlemail.com>
2021-12-23 11:36:10 +0000

    lesson 6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -502,7 +502,7 @@
                         </svg>
 
                         <a href="https://laravel.bigcartel.com" class="ml-1 underline">
-                            Shopppppping!
+                            {{$name}}
                         </a>
 
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
